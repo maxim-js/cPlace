f52577c77a6c0c21d6b8431b72595423ca07ed90
maxim-js <makssp7@inbox.ru>
2020-08-07 12:27:03 +0800

    second commit

--- src/components/tuning/tuning.js
+++ src/components/tuning/tuning.js
@@ -6,7 +6,20 @@ export default class Tuning extends Component {
 
     render() {
       return (
-        <div className="tuning"></div>
+        <div className="tuning">
+            <span className="tuning__title">Options:</span>
+
+            <div className="input-group">
+                <div className="input-group-prepend">
+                    <div className="input-group-text">
+                        <input type="checkbox" aria-label="Checkbox for following text input"/>
+                    </div>
+                </div>
+                <div type="text" class="form-control">
+                    Добавить каталог популярных мест
+                </div>
+            </div>
+        </div>
       );
     }
 }
